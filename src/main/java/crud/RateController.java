fa3af1e3601e9f3b227f6149711b48a1757f5954
bylinovich <damovik1@gmail.com>
2016-11-11 23:51:06 +0300

    add RateController

---
+++ src/main/java/crud/RateController.java
@@ -0,0 +1,27 @@
+package crud;
+
+import entity.Rate;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Влад on 11.11.2016.
+ */
+public class RateController {
+    public List<Rate> getAllRates(){
+
+        return null;
+    }
+
+    public List<Rate> getRatesBetweenDates(Date fromDate, Date toDate){
+
+        return null;
+    }
+
+    public Rate getCurrentRate()
+    {
+        
+        return null;
+    }
+}
