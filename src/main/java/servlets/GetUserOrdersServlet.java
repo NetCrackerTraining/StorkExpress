73e4b5c98f776862cc6795ae1249224d14d49c42
bylinovich <damovik1@gmail.com>
2016-11-12 00:42:08 +0300

    add Empty Servlets

---
+++ src/main/java/servlets/GetUserOrdersServlet.java
@@ -0,0 +1,27 @@
+package servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Влад on 12.11.2016.
+ */
+@WebServlet("/UserOrders")
+public class GetUserOrdersServlet extends BaseHttpServlet {
+    protected void process(HttpServletRequest request, HttpServletResponse response) {
+        response.setStatus(200);
+
+        PrintWriter pw = null;
+        try {
+            pw = response.getWriter();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        pw.println("Ok");
+        pw.close();
+    }
+}
