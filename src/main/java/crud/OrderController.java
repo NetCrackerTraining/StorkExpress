f8618fd6e706ca3ccf243ece2db2e2f43ce4fa31
bylinovich <damovik1@gmail.com>
2016-11-12 00:15:42 +0300

    add OrderController

---
+++ src/main/java/crud/OrderController.java
@@ -0,0 +1,26 @@
+package crud;
+
+import entity.Order;
+
+import java.util.List;
+
+/**
+ * Created by Влад on 12.11.2016.
+ */
+public class OrderController {
+    public List<Order> getAllOrders(){
+        return null;
+    }
+    public boolean addOrder(Order order){
+        return false;
+    }
+    public List<Order> getUserOrders(long UserId){
+        return null;
+    }
+    public boolean deleteOrder(long OrderId){
+        return false;
+    }
+    public boolean updateOrder(Order order){
+        return false;
+    }
+}
