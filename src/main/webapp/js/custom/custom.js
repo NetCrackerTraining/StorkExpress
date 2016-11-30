6e97a82548dd5485d3df1fb61614c02bb7e5a037
sashagam <sashagam2@gmail.com>
2016-11-30 04:02:41 +0300

    custom.js changed

--- src/main/webapp/js/custom/custom.js
+++ src/main/webapp/js/custom/custom.js
@@ -2,23 +2,8 @@
  * Created by Alexandr on 24.11.2016.
  */
 function phoneCheck(input) {
-    ch = input.value.replace(/[^\d+ ]/g, '');
+    ch = input.value.replace(/[^\d]/g, '');
     document.getElementById('phone_number').onkeypress = function (e) {
-    }
-    input.value = ch;
-}
-function phoneCheck2(input) {
-    ch = input.value.replace(/[^\d+ ]/g, '');
-    document.getElementById('newPhoneNumber').onkeypress = function (e) {
-    }
-    input.value = ch;
-}
-function weightCheck(input) {
-    ch = input.value.replace(/[^\d,.]/g, '');
-    document.getElementById('weight').onkeypress = function (e) {
-        if (this.value.indexOf(".") != '-1' || this.value.indexOf(",") != '-1') {
-            return !(/[.,]/.test(String.fromCharCode(e.charCode)));
-        }
     }
     input.value = ch;
 }
