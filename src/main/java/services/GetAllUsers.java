a6c5b0279f4cb0880a3a2fc42e6e34ac8da288ef
bylinovich <damovik1@gmail.com>
2016-12-07 12:19:45 +0300

    added test method to web service

--- src/main/java/services/GetAllUsers.java
+++ src/main/java/services/GetAllUsers.java
@@ -1,7 +1,15 @@
 package services;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
 /**
  * Created by Влад on 06.12.2016.
  */
+@WebService
 public class GetAllUsers {
+    @WebMethod
+    public String getTestString(){
+        return "test string";
+    }
 }
