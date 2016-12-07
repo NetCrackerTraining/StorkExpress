ab57b0898b098f5eb653d065e09471dd690a45dc
bylinovich <damovik1@gmail.com>
2016-12-07 13:00:16 +0300

    added getTestUser method

--- src/main/java/services/GetAllUsers.java
+++ src/main/java/services/GetAllUsers.java
@@ -1,7 +1,10 @@
 package services;
 
+import services.model.User;
+
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 
 /**
@@ -13,8 +16,21 @@ public class GetAllUsers {
     public String getTestString(){
         return "test string";
     }
+
+
     @WebMethod
     public String sayHelloTo(@WebParam(name = "text") String text) {
         return "Hello to " + text;
+    }
+
+
+    @WebMethod
+    public @WebResult(name="user")User getTestUser(){
+        User user = new User();
+        user.setEmail("email@email.ru");
+        user.setPassword("1111");
+        user.setUsername("username");
+
+        return user;
     }
 }
