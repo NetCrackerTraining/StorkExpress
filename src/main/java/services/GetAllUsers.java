fe4826ad057e1c5ef0a43a466c019d95b1145cca
bylinovich <damovik1@gmail.com>
2016-12-07 12:37:32 +0300

    added test method with @WebParam

--- src/main/java/services/GetAllUsers.java
+++ src/main/java/services/GetAllUsers.java
@@ -1,6 +1,7 @@
 package services;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 /**
@@ -11,5 +12,9 @@ public class GetAllUsers {
     @WebMethod
     public String getTestString(){
         return "test string";
+    }
+    @WebMethod
+    public String sayHelloTo(@WebParam(name = "text") String text) {
+        return "Hello to " + text;
     }
 }
