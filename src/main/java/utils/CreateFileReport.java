c38cb38cdae84cc101247b4386abcdd10238510f
valynkoandrei <valynkoandrei@gmail.com>
2016-12-10 19:56:00 +0300

    create XLS file and method writeBestUsers

---
+++ src/main/java/utils/CreateFileReport.java
@@ -0,0 +1,48 @@
+package utils;
+
+import crud.Reports;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import java.io.*;
+
+/**
+ * Created by Андрей on 09.12.2016.
+ */
+public class CreateFileReport {
+
+    public static void main(String[] args) throws IOException {
+        Reports reports = new Reports();
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("Sample sheet");
+
+        System.out.println(writeBestUsers(reports.bestUsers(),sheet));
+
+        try {
+            FileOutputStream out =
+                    new FileOutputStream(new File("new.xls"));
+            workbook.write(out);
+            out.close();
+            System.out.println("Excel written successfully..");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static boolean writeBestUsers(String[][] result, HSSFSheet sheet){
+        for (int i = 0; i < 21; i++) {
+            Row row = sheet.createRow(i);
+            for (int j=0; j < 2; j++) {
+                Cell cell = row.createCell(j);
+                cell.setCellValue(result[j][i]);
+            }
+        }
+        return true;
+    }
+
+}
