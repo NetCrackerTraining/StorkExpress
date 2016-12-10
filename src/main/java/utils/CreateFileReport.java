1a785b9a4d5572c538917f9517b69d72a58922a3
valynkoandrei <valynkoandrei@gmail.com>
2016-12-10 20:05:10 +0300

    add method writeYearReport

--- src/main/java/utils/CreateFileReport.java
+++ src/main/java/utils/CreateFileReport.java
@@ -17,8 +17,8 @@ public class CreateFileReport {
         HSSFWorkbook workbook = new HSSFWorkbook();
         HSSFSheet sheet = workbook.createSheet("Sample sheet");
 
-        System.out.println(writeBestUsers(reports.bestUsers(),sheet));
-
+//        System.out.println(writeBestUsers(reports.bestUsers(),sheet));
+        System.out.println(writeYearReport(reports.yearReport(),sheet));
         try {
             FileOutputStream out =
                     new FileOutputStream(new File("new.xls"));
@@ -38,6 +38,17 @@ public class CreateFileReport {
         for (int i = 0; i < 21; i++) {
             Row row = sheet.createRow(i);
             for (int j=0; j < 2; j++) {
+                Cell cell = row.createCell(j);
+                cell.setCellValue(result[j][i]);
+            }
+        }
+        return true;
+    }
+
+    public static boolean writeYearReport(String[][] result, HSSFSheet sheet) {
+        for (int i = 0; i < 13; i++) {
+            Row row = sheet.createRow(i);
+            for (int j=0; j < 3; j++) {
                 Cell cell = row.createCell(j);
                 cell.setCellValue(result[j][i]);
             }
