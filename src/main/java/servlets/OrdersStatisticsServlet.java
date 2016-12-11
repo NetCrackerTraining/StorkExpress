daa2b2102e632e88cfe59a3fbfc93f29e6a7dc1e
bylinovich <damovik1@gmail.com>
2016-12-11 20:30:59 +0300

    added OrdersStatisticsServlet

---
+++ src/main/java/servlets/OrdersStatisticsServlet.java
@@ -0,0 +1,16 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Влад on 11.12.2016.
+ */
+public class OrdersStatisticsServlet extends BaseHttpServlet {
+    @Override
+    protected void process(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+    }
+}
